05bb96f7f5ceb3a4707ef57bdb2181cfdf39d01f
Lethal10 <lethal9@protonmail.ch>
2020-01-10 14:11:42 +0000

    Modified Version

--- src/com/lethal9/getorganized/Main.java
+++ src/com/lethal9/getorganized/Main.java
@@ -3,6 +3,37 @@ package com.lethal9.getorganized;
 public class Main {
 
     public static void main(String[] args) {
+      double val1 = 100.0d;
+      double val2 = 50.0d ;
+      double result;
+      char opCode = 'd';
+
+      if (opCode == 'm')
+          result = val1 + val2;
+
+      else if (opCode == 's' )
+          result = val1 - val2;
+
+      else if (opCode == 'd' ) {
+          result = val2 != 0.0d ? val1 / val2 : 0.0d;
+
+      /*if (val2 != 0.0d)
+          result = val1 / val2 ;
+      else
+          result = 0.0d;
+
+       */}
+
+      else if (opCode == 'm' )
+          result = val1 * val2;
+
+
+      else {
+          System.out.println("Error - invalid opCode");
+          result = 0.0d;
+      }
+      System.out.println(result);
+
 
     }
 }
